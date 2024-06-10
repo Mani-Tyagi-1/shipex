51f9e5975070a0026f5360b857f95c48f57bdfa6
Riya <riyathakurprof@gmail.com>
2024-06-10 10:30:21 +0530

    verify-name page done

--- app/(pages)/(KYC)/verifyotp/page.jsx
+++ app/(pages)/(KYC)/verifyotp/page.jsx
@@ -1,53 +1,53 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 
 const verifyOtp = () => {
   return (
-    <div className="w-full min-h-screen bg-[#D3D3D3] flex justify-center items-center">
-      <div className="w-[25rem]  h-[27rem] bg-white shadow-lg  p-7 pt-8 rounded-lg">
-        <p className="py-2 mb-4 font-bold text-gray-500 text-[.85rem]">
+    <div className='w-full min-h-screen bg-[#D3D3D3] flex justify-center items-center'>
+      <div className='w-[25rem]  h-[27rem] bg-white shadow-lg  p-7 pt-8 rounded-lg'>
+        <p className='py-2 mb-4 font-bold text-gray-500 text-[.85rem]'>
           Contact Details
         </p>
-        <div className="w-full h-full flex flex-col ">
-          <h4 className="font-semibold my-4 text-[1rem]">
-            Enter the OTP {" "}
-          </h4>
-          <div className="w-full ">
+        <div className='w-full h-full flex flex-col '>
+          <h4 className='font-semibold my-4 text-[1rem]'>Enter the OTP </h4>
+          <div className='w-full '>
             <input
-              type="mobile"
-              value=""
-              className="w-[100%] border border-gray-300 rounded p-2 outline-none"
-              placeholder="Enter Mobile Number"
-                      />
-                      <div className="w-full flex justify-end px-5 hover:underline">
-                          <Link href="#" className="text-blue-400 text-[.85rem] font-bold">Resend OTP</Link>
-                      </div>
+              type='mobile'
+              value=''
+              className='w-[100%] border border-gray-300 rounded p-2 outline-none'
+              placeholder='Enter Mobile Number'
+            />
+            <div className='w-full flex justify-end px-5 hover:underline'>
+              <Link href='#' className='text-blue-400 text-[.85rem] font-bold'>
+                Resend OTP
+              </Link>
+            </div>
           </div>
 
-          <div className="w-full flex flex-col justify-center mt-[8rem]">
-            <div className="w-full h-full mb-2 flex justify-center">
-              <p className="font-semibold text-center text-[.65rem] text-gray-400">
-                By clicking &quot;Submit OTP&quot,I agree{" "}
-                <Link href="#" className="text-[#00c27c]">
-                  {" "}
+          <div className='w-full flex flex-col justify-center mt-[8rem]'>
+            <div className='w-full h-full mb-2 flex justify-center'>
+              <p className='font-semibold text-center text-[.65rem] text-gray-400'>
+                By clicking &quot;Submit OTP&quot,I agree{' '}
+                <Link href='#' className='text-[#00c27c]'>
+                  {' '}
                   Terms & Conditions
                 </Link>
-                ,{" "}
-                <Link href="#" className="text-[#00c27c]">
+                ,{' '}
+                <Link href='#' className='text-[#00c27c]'>
                   Privacy Policy
-                </Link>{" "}
-                and{" "}
-                <Link href="#" className="text-[#00c27c]">
-                  {" "}
+                </Link>{' '}
+                and{' '}
+                <Link href='#' className='text-[#00c27c]'>
+                  {' '}
                   Service Agreement
-                </Link>{" "}
+                </Link>{' '}
               </p>
             </div>
-            <div className="w-full flex justify-center">
-              <div className="border flex items-center justify-center  w-[20rem]  cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] ">
+            <div className='w-full flex justify-center'>
+              <div className='border flex items-center justify-center  w-[20rem]  cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
                 <Link
-                  href="/verifyotp"
-                  className=" p-3 !font-medium dark:border-defaultborder/10"
+                  href='/verifyotp'
+                  className=' p-3 !font-medium dark:border-defaultborder/10'
                 >
                   Submit OTP
                 </Link>
