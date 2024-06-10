34cd2f21eb1d29a190ee3492b69be5fa0fca4d48
Riya <riyathakurprof@gmail.com>
2024-06-10 17:16:12 +0530

    email updated page

--- app/(pages)/(KYC)/emailupdate/page.jsx
+++ app/(pages)/(KYC)/emailupdate/page.jsx
@@ -1,42 +1,32 @@
 import Link from 'next/link';
 import React from 'react';
 
-const verifypan = () => {
+const emailupdate = () => {
   return (
     <div className='w-full min-h-screen bg-[#D3D3D3] flex justify-center items-center'>
-      <div className='w-[20rem]  h-[25rem] bg-white shadow-lg  p-7 pt-8 rounded-lg'>
-        <h1 className='py-2 mb-4 font-bold text-blck-500 text-[.85rem]'>
-          Verify PAN
-        </h1>
+      <div className='w-[25rem]  h-[27rem] bg-white shadow-lg  p-7 pt-8 rounded-lg'>
+        <p className='py-2 mb-4 font-bold text-gray-500 text-[.85rem]'>Back</p>
         <div className='w-full h-full flex flex-col '>
-          <h4 className='font-semibold my-2 text-[.90rem]'>PAN</h4>
+          <h4 className='font-semibold my-4 text-[1rem]'>
+            Add your email address to receive account updates
+          </h4>
           <div className='w-full '>
             <input
               type='mobile'
               value=''
               className='w-[100%] border border-gray-300 rounded p-2 outline-none'
-              placeholder='Ex. ABCP1234A1'
-            />
-          </div>
-          <div className='w-full h-full flex flex-col '></div>
-          <h4 className='font-semibold my-1 text-[.90rem]'>Name</h4>
-          <div className='w-full '>
-            <input
-              type='mobile'
-              value=''
-              className='w-[100%] border border-gray-300 rounded p-2 outline-none'
-              placeholder='Name'
+              placeholder='Ex. name@gmail.com'
             />
           </div>
 
-          <div className='w-full flex flex-col justify-center mt-[6rem]'>
+          <div className='w-full flex flex-col justify-center mt-[8rem]'>
             <div className='w-full flex justify-center'>
               <div className='border flex items-center justify-center  w-[20rem]  cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
                 <Link
                   href='/verifyotp'
                   className=' p-3 !font-medium dark:border-defaultborder/10'
                 >
-                  Verify
+                  Send OTP
                 </Link>
               </div>
             </div>
@@ -47,4 +37,4 @@ const verifypan = () => {
   );
 };
 
-export default verifypan;
+export default emailupdate;
