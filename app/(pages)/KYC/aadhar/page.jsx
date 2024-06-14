f69a1150bcc29ec4f3bca5b909870c54cce405a8
ManiTyagi <manityagi1919@gmail.com>
2024-06-14 19:29:18 +0530

    updated aadhar

--- app/(pages)/KYC/aadhar/page.jsx
+++ app/(pages)/KYC/aadhar/page.jsx
@@ -5,40 +5,48 @@ import React from 'react';
 
 const AadhaarDetails = () => {
   return (
-    <div className='w-full min-h-screen bg-[#D3D3D3] flex justify-center items-center'>
-      <div className='w-[30rem]  h-[27rem] bg-white shadow-lg  p-7 pt-8 rounded-lg'>
-        <p className='py-2 mb-4 font-bold text-gray-500 text-[.85rem]'>Back</p>
-        <div className='w-full h-full flex flex-col '>
-          <h4 className='font-semibold p-2 my-1 text-[1rem]'>Aadhaar Details </h4>
-          <div className='w-full '>
+    <div className="w-full min-h-screen bg-[#D3D3D3] flex justify-center items-center">
+      <div className="w-[30rem]  h-[27rem] bg-white shadow-lg  p-7 pt-8 rounded-lg">
+        <p className="py-2 mb-4 font-bold text-gray-500 text-[.85rem]">Back</p>
+        <div className="w-full h-full flex flex-col ">
+          <h4 className="font-semibold p-2 my-1 text-[1rem]">
+            Aadhaar Details{" "}
+          </h4>
+          <div className="w-full ">
+            <select
+              type="text"
+              id="verify-type"
+              name="Verify Type"
+              class="w-full p-3 border rounded-lg text-sm outline-[#00c27c] outline-[1px]"
+              placeholder=""
+              required=""
+            >
+              <option value="instant">Instant</option>
+              <option value="mannual">Mannual </option>
+            </select>
+
             <input
-              type='mobile'
+              type="number"
               value={AadhaarDetails}
-              className='w-[95%] border border-gray-300 rounded p-2 my-4 outline-none'
-              placeholder=''
-            />
-            <input
-              type='mobile'
-              value={AadhaarDetails}
-              className='w-[95%] border border-gray-300 rounded p-3 outline-none'
-              placeholder='Enter Aadhaar Number'
+              className="w-full border border-gray-300 mt-4 rounded-lg p-2 outline-none"
+              placeholder="Enter Aadhaar Number"
             />
           </div>
 
-          <div className='w-full  justify-center'>
-            <div className='border flex items-center justify-center  w-full  mt-[3rem] cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
+          <div className="w-full  justify-center">
+            <div className="border flex items-center justify-center  w-full  mt-[3rem] cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] ">
               <Link
-                href='/verifyotp'
-                className=' !font-medium dark:border-defaultborder/10'
+                href="/verifyotp"
+                className=" !font-medium dark:border-defaultborder/10"
               >
                 Send OTP
               </Link>
             </div>
 
-            <div className='border flex items-center justify-center  w-full  mt-[1rem] cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
+            <div className="border flex items-center justify-center  w-full  mt-[1rem] cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] ">
               <Link
-                href='/verifyotp'
-                className=' !font-medium dark:border-defaultborder/10'
+                href="/verifyotp"
+                className=" !font-medium dark:border-defaultborder/10"
               >
                 Update
               </Link>
