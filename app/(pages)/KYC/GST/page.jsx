497dd9ce56947b714a77e870e5a406451e953a6b
Riya <riyathakurprof@gmail.com>
2024-06-14 19:04:19 +0530

    GST and aadhaar details created

--- app/(pages)/KYC/GST/page.jsx
+++ app/(pages)/KYC/GST/page.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
 
 const GST = () => {
   return (
-    <div>GST</div>
-  )
-}
+    <div className='w-full min-h-screen bg-[#D3D3D3] flex justify-center items-center'>
+      <div className='w-[25rem]  h-[23rem] bg-white shadow-lg  p-7 pt-8 rounded-lg'>
+        <p className='py-2 mb-4 font-bold text-gray-500 text-[.85rem]'>Back</p>
+        <div className='w-full h-full flex flex-col '>
+          <h4 className='font-semibold my-4 text-[1rem]'>GST Details </h4>
+          <div className='w-full '>
+            <input
+              type='mobile'
+              value={GST}
+              className='w-[95%] border border-gray-300 rounded p-2 outline-none'
+              placeholder='Enter GST Number'
+            />
+          </div>
 
-export default GST
+          <div className='w-full  justify-center'>
+            <div className='border flex items-center justify-center  w-full  mt-[3rem] cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
+              <Link
+                href='/verifyotp'
+                className=' !font-medium dark:border-defaultborder/10'
+              >
+                Verify GST
+              </Link>
+            </div>
+
+            <div className='border flex items-center justify-center  w-full  mt-[1rem] cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
+              <Link
+                href='/verifyotp'
+                className=' !font-medium dark:border-defaultborder/10'
+              >
+                Update
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GST;
--- app/(pages)/KYC/aadhar/page.jsx
+++ app/(pages)/KYC/aadhar/page.jsx
@@ -1,34 +1,47 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
 
-const Aadhar = () => {
+const AadhaarDetails = () => {
   return (
     <div className='w-full min-h-screen bg-[#D3D3D3] flex justify-center items-center'>
-      <div className='w-[25rem]  h-[20rem] bg-white shadow-lg  p-7 pt-8 rounded-lg'>
-        <h1 className='py-2 mb-4 font-bold text-blck-500 text-[.85rem]'>
-          Verify Aadhaar
-        </h1>
+      <div className='w-[30rem]  h-[27rem] bg-white shadow-lg  p-7 pt-8 rounded-lg'>
+        <p className='py-2 mb-4 font-bold text-gray-500 text-[.85rem]'>Back</p>
         <div className='w-full h-full flex flex-col '>
-          <h4 className='font-semibold my-4 text-[1rem]'>Aadhaar Number</h4>
+          <h4 className='font-semibold p-2 my-1 text-[1rem]'>Aadhaar Details </h4>
           <div className='w-full '>
             <input
               type='mobile'
-              value=''
-              className='w-[100%] border border-gray-300 rounded p-2 outline-none'
-              placeholder='Ex. XXXXXXX1234'
+              value={AadhaarDetails}
+              className='w-[95%] border border-gray-300 rounded p-2 my-4 outline-none'
+              placeholder=''
+            />
+            <input
+              type='mobile'
+              value={AadhaarDetails}
+              className='w-[95%] border border-gray-300 rounded p-3 outline-none'
+              placeholder='Enter Aadhaar Number'
             />
           </div>
 
-          <div className='w-full flex flex-col justify-center mt-[3rem]'>
-            <div className='w-full flex justify-center'>
-              <div className='border flex items-center justify-center  w-full  cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
-                <Link
-                  href='aadhar/verifyotp'
-                  className=' p-3 !font-medium dark:border-defaultborder/10'
-                >
-                  Verify
-                </Link>
-              </div>
+          <div className='w-full  justify-center'>
+            <div className='border flex items-center justify-center  w-full  mt-[3rem] cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
+              <Link
+                href='/verifyotp'
+                className=' !font-medium dark:border-defaultborder/10'
+              >
+                Send OTP
+              </Link>
+            </div>
+
+            <div className='border flex items-center justify-center  w-full  mt-[1rem] cursor-pointer hover:bg-[#43aa84] bg-[#00c27c] text-white  dark:border-defaultborder/10 h-[2.5rem] rounded-[2rem] '>
+              <Link
+                href='/verifyotp'
+                className=' !font-medium dark:border-defaultborder/10'
+              >
+                Update
+              </Link>
             </div>
           </div>
         </div>
@@ -37,4 +50,4 @@ const Aadhar = () => {
   );
 };
 
-export default Aadhar;
+export default AadhaarDetails;
